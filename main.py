523078831e5295aabd73cddaa2cf705630dec1db
DHEERAJ NANDA <dheerajnanda20@gmail.com>
2025-09-23 16:23:28 +0530

    Add files via upload
    snake , water and gun game 
    using python

---
+++ main.py
@@ -0,0 +1,34 @@
+'''
+1 for snake
+-1 for water
+0 for gun 
+'''
+import random 
+computer = random.choice([1,-1,0])
+youstr = input("Enter your choice:")
+youdict = { "s":1 ,  "w":-1 ,  "g":0 }  
+reversedict = {1:"snake",-1:"water",0:"gun"}  
+you = youdict[youstr]  
+print(f"you choose {reversedict[you]}\ncomputer choose {reversedict[computer]}")
+if(computer==you):
+    print("its a draw")  
+else:
+    if(computer==-1 and you==1):
+        print("You win!")
+    elif(computer==-1 and you==0):
+        print("you lose!") 
+
+    elif(computer==1 and you==-1):
+        print("you lose!")
+    elif(computer==1 and you==0):
+        print("you Win!")  
+    elif(computer==0 and you==-1):
+        print("You win!") 
+    elif(computer==0 and you==1):
+        print("You lost!") 
+
+    else:
+        print("something went wrong!")  
+
+
+print("GAME (SNAKE,WATER AND GUN)") 
